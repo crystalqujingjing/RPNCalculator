956f99cf3057dc3e95cad563374499851f3e0f29
crystalqujingjing <crystalqujingjing@gmail.com>
2020-02-28 23:17:02 +0800

    Update RPNCalculator.java

--- RPNCalculator.java
+++ RPNCalculator.java
@@ -3,5 +3,5 @@ import java.util.LinkedList;
 public interface RPNCalculator<T> {
 	public LinkedList<T> calculate(String input) throws Exception;
 	
-	public String display();
+	public String display(LinkedList<T> stack);
 }
